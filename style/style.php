2d73429a631cc2cf2d18a56886352e6965abdf92
Dowzz <michel.barny@outlook.fr>
2022-04-02 07:55:03 +0200

    css partie 2
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- style/style.php
+++ style/style.php
@@ -4,11 +4,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 <script src="./js/myscript.js"></script>
 <link rel="stylesheet" href="./css/bootstrap.min.css">
-<link href="./css/style.css" rel='stylesheet' type='text/css' />
+
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Hubballi&family=Hurricane&display=swap" rel="stylesheet">
 <link href="./css/Password_Checker.css" rel='stylesheet' type='text/css' />
+<link href="./css/style.css" rel='stylesheet' type='text/css' />
 <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
