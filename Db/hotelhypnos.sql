f1c412d2e506cdc3734aa167bd90b7f4dabb49b0
Dowzz <michel.barny@outlook.fr>
2022-04-11 15:13:05 +0200

    maj bdd OK
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- Db/hotelhypnos.sql
+++ Db/hotelhypnos.sql
@@ -1,273 +1,13 @@
-SET
-  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET
-  time_zone = "+00:00";
--- Structure de la table `contact`
-  --
-  DROP TABLE IF EXISTS `contact`;
-CREATE TABLE IF NOT EXISTS `contact` (
-    `contactId` int NOT NULL AUTO_INCREMENT,
-    `email` varchar(50) NOT NULL,
-    `nom` varchar(255) NOT NULL,
-    `prenom` varchar(255) NOT NULL,
-    `sujet` varchar(255) NOT NULL,
-    `message` varchar(255) NOT NULL,
-    `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`contactId`)
-  ) ENGINE = MyISAM AUTO_INCREMENT = 31 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
--- --------------------------------------------------------
-  --
-  -- Structure de la table `etablissement`
-  --
-  DROP TABLE IF EXISTS `etablissement`;
-CREATE TABLE IF NOT EXISTS `etablissement` (
-    `etabId` int NOT NULL AUTO_INCREMENT,
-    `nom` varchar(50) NOT NULL,
-    `ville` varchar(50) NOT NULL,
-    `adresse` varchar(50) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `userId` int NOT NULL,
-    PRIMARY KEY (`etabId`),
-    UNIQUE KEY `userId` (`userId`)
-  ) ENGINE = MyISAM AUTO_INCREMENT = 171 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
---
-  -- Déchargement des données de la table `etablissement`
-  --
-INSERT INTO
-  `etablissement` (
-    `etabId`,
-    `nom`,
-    `ville`,
-    `adresse`,
-    `description`,
-    `userId`
-  )
-VALUES
-  (
-    160,
-    'Le relais du chateau',
-    'Chalon',
-    '15 rue de l\'eglise',
-    'Un joli petit hotel situé près d\'une rivière a 5 km du centre ville',
-    26
-  ),
-  (
-    170,
-    'La tour du pin',
-    'Marseille',
-    '25 impasse du bourg',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aspernatur molestias cum, optio totam doloremque nobis quisquam quam assumenda expedita magnam, veritatis sint asperiores itaque dignissimos eos quis. Vitae, minus.',
-    54
-  ),
-  (
-    169,
-    'La maison de papier',
-    'Paris',
-    '17 route de la paix',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aspernatur molestias cum, optio totam doloremque nobis quisquam quam assumenda expedita magnam, veritatis sint asperiores itaque dignissimos eos quis. Vitae, minus.',
-    58
-  ),
-  (
-    167,
-    'l\'Hotel de ville',
-    'Nantes',
-    '45 rue de la mairie',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quia perspiciatis temporibus saepe eum corporis sed facere tempore in. Cumque mollitia labore quaerat corporis libero neque officia fugit in error.',
-    56
-  ),
-  (
-    168,
-    'La maison de papier',
-    'Caen',
-    '12 avenue du 6 juin',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aspernatur molestias cum, optio totam doloremque nobis quisquam quam assumenda expedita magnam, veritatis sint asperiores itaque dignissimos eos quis. Vitae, minus.',
-    59
-  );
--- --------------------------------------------------------
-  --
-  -- Structure de la table `imagegal`
-  --
-  DROP TABLE IF EXISTS `imagegal`;
-CREATE TABLE IF NOT EXISTS `imagegal` (
-    `id_image` int NOT NULL AUTO_INCREMENT,
-    `link` varchar(255) DEFAULT NULL,
-    `suiteId` int NOT NULL,
-    PRIMARY KEY (`id_image`),
-    KEY `suiteId` (`suiteId`)
-  ) ENGINE = MyISAM AUTO_INCREMENT = 14 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
---
-  -- Déchargement des données de la table `imagegal`
-  --
-INSERT INTO
-  `imagegal` (`id_image`, `link`, `suiteId`)
-VALUES
-  (
-    1,
-    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
-    59
-  ),
-  (
-    2,
-    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
-    59
-  ),
-  (
-    3,
-    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
-    59
-  ),
-  (
-    4,
-    'https://webzine.okeenea.com/wp-content/uploads/2018/07/chambre-h%C3%B4tel-PMR-obligations-normes-loi-accessibilit%C3%A9.jpg',
-    59
-  ),
-  (
-    5,
-    'https://webzine.okeenea.com/wp-content/uploads/2018/07/chambre-h%C3%B4tel-PMR-obligations-normes-loi-accessibilit%C3%A9.jpg',
-    60
-  ),
-  (
-    6,
-    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
-    60
-  ),
-  (
-    7,
-    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
-    61
-  ),
-  (
-    8,
-    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
-    61
-  ),
-  (
-    9,
-    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
-    60
-  ),
-  (
-    10,
-    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
-    59
-  ),
-  (
-    11,
-    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
-    61
-  ),
-  (12, '', 61),
-  (
-    13,
-    'https://webzine.okeenea.com/wp-content/uploads/2018/07/chambre-h%C3%B4tel-PMR-obligations-normes-loi-accessibilit%C3%A9.jpg',
-    60
-  );
--- --------------------------------------------------------
-  --
-  -- Structure de la table `reservation`
-  --
-  DROP TABLE IF EXISTS `reservation`;
-CREATE TABLE IF NOT EXISTS `reservation` (
-    `resId` int NOT NULL AUTO_INCREMENT,
-    `suiteId` int NOT NULL,
-    `startdate` date NOT NULL,
-    `enddate` date NOT NULL,
-    `etabId` int NOT NULL,
-    `userId` int NOT NULL,
-    PRIMARY KEY (`resId`),
-    KEY `suiteId` (`suiteId`),
-    KEY `etabId` (`etabId`),
-    KEY `userId` (`userId`)
-  ) ENGINE = MyISAM AUTO_INCREMENT = 56 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
---
-  -- Déchargement des données de la table `reservation`
-  --
-INSERT INTO
-  `reservation` (
-    `resId`,
-    `suiteId`,
-    `startdate`,
-    `enddate`,
-    `etabId`,
-    `userId`
-  )
-VALUES
-  (55, 45, '2022-03-28', '2022-03-31', 160, 53);
--- --------------------------------------------------------
-  --
-  -- Structure de la table `suite`
-  --
-  DROP TABLE IF EXISTS `suite`;
-CREATE TABLE IF NOT EXISTS `suite` (
-    `suiteId` int NOT NULL AUTO_INCREMENT,
-    `titre` varchar(50) NOT NULL,
-    `image` varchar(255) NOT NULL,
-    `descriptif` varchar(255) NOT NULL,
-    `prix` decimal(15, 2) NOT NULL,
-    `booking` varchar(100) DEFAULT NULL,
-    `etabId` int NOT NULL,
-    PRIMARY KEY (`suiteId`),
-    KEY `etabId` (`etabId`)
-  ) ENGINE = MyISAM AUTO_INCREMENT = 62 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
---
-  -- Déchargement des données de la table `suite`
-  --
-INSERT INTO
-  `suite` (
-    `suiteId`,
-    `titre`,
-    `image`,
-    `descriptif`,
-    `prix`,
-    `booking`,
-    `etabId`
-  )
-VALUES
-  (
-    59,
-    'Suite parentale',
-    'https://www.deco.fr/sites/default/files/styles/full_1200x630/public/migration-images/101525.jpg?itok=xm0oumy4',
-    'Une jolie suite parentale avec deux lits doubles',
-    '25.00',
-    'Book',
-    160
-  ),
-  (
-    60,
-    'Suite parentale',
-    'https://www.deco.fr/sites/default/files/styles/full_1200x630/public/migration-images/101525.jpg?itok=xm0oumy4',
-    'Une jolie suite parentale avec deux lits doubles',
-    '25.00',
-    'Book',
-    160
-  ),
-  (
-    61,
-    'Suite parentale',
-    'https://www.deco.fr/sites/default/files/styles/full_1200x630/public/migration-images/101525.jpg?itok=xm0oumy4',
-    'Une jolie suite parentale avec deux lits doubles',
-    '25.00',
-    'Book',
-    160
-  );
--- --------------------------------------------------------
-  --
-  -- Structure de la table `utilisateur`
-  --
-  DROP TABLE IF EXISTS `utilisateur`;
+DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-    `userId` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `prenom` varchar(50) NOT NULL,
-    `email` varchar(50) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `role` varchar(50) NOT NULL,
-    PRIMARY KEY (`userId`)
-  ) ENGINE = MyISAM AUTO_INCREMENT = 61 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
---
-  -- Déchargement des données de la table `utilisateur`
-  --
+  `userId` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE = MyISAM AUTO_INCREMENT = 61 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 INSERT INTO
   `utilisateur` (
     `userId`,
@@ -357,4 +97,222 @@ VALUES
     'client@test.com',
     '$2y$10$/AibYsYLbdbcQ8zdir9At.b1TLwRg7Ym0x.dpp28Naoh0qXFO5sr.',
     'client'
+  );
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+    `contactId` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(50) NOT NULL,
+    `nom` varchar(255) NOT NULL,
+    `prenom` varchar(255) NOT NULL,
+    `sujet` varchar(255) NOT NULL,
+    `message` varchar(255) NOT NULL,
+    `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`contactId`)
+  ) ENGINE = MyISAM AUTO_INCREMENT = 31 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `imagegal`;
+CREATE TABLE IF NOT EXISTS `imagegal` (
+    `id_image` int NOT NULL AUTO_INCREMENT,
+    `link` varchar(255) DEFAULT NULL,
+    `suiteId` int NOT NULL,
+    PRIMARY KEY (`id_image`),
+    KEY `suiteId` (`suiteId`)
+  ) ENGINE = MyISAM AUTO_INCREMENT = 14 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+INSERT INTO
+  `imagegal` (`id_image`, `link`, `suiteId`)
+VALUES
+  (
+    1,
+    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
+    59
+  ),
+  (
+    2,
+    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
+    59
+  ),
+  (
+    3,
+    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
+    59
+  ),
+  (
+    4,
+    'https://webzine.okeenea.com/wp-content/uploads/2018/07/chambre-h%C3%B4tel-PMR-obligations-normes-loi-accessibilit%C3%A9.jpg',
+    59
+  ),
+  (
+    5,
+    'https://webzine.okeenea.com/wp-content/uploads/2018/07/chambre-h%C3%B4tel-PMR-obligations-normes-loi-accessibilit%C3%A9.jpg',
+    60
+  ),
+  (
+    6,
+    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
+    60
+  ),
+  (
+    7,
+    'https://www.hotel-diana-dauphine.com/media/cache/jadro_resize/rc/PHWZk38w1625734188/jadroRoot/medias/5658345e8f976/chambre-1.jpg',
+    61
+  ),
+  (
+    8,
+    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
+    61
+  ),
+  (
+    9,
+    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
+    60
+  ),
+  (
+    10,
+    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
+    59
+  ),
+  (
+    11,
+    'https://www.beaujoire-hotel.com/usermedia/photo-636250158080377143-1.jpg?dummy=0&h=800',
+    61
+  ),
+  (12, '', 61),
+  (
+    13,
+    'https://webzine.okeenea.com/wp-content/uploads/2018/07/chambre-h%C3%B4tel-PMR-obligations-normes-loi-accessibilit%C3%A9.jpg',
+    60
+  );
+DROP TABLE IF EXISTS `reservation`;
+CREATE TABLE IF NOT EXISTS `reservation` (
+    `resId` int NOT NULL AUTO_INCREMENT,
+    `suiteId` int NOT NULL,
+    `startdate` date NOT NULL,
+    `enddate` date NOT NULL,
+    `etabId` int NOT NULL,
+    `userId` int NOT NULL,
+    PRIMARY KEY (`resId`),
+    KEY `suiteId` (`suiteId`),
+    KEY `etabId` (`etabId`),
+    KEY `userId` (`userId`)
+  ) ENGINE = MyISAM AUTO_INCREMENT = 56 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+INSERT INTO
+  `reservation` (
+    `resId`,
+    `suiteId`,
+    `startdate`,
+    `enddate`,
+    `etabId`,
+    `userId`
+  )
+VALUES
+  (55, 45, '2022-03-28', '2022-03-31', 160, 53);
+DROP TABLE IF EXISTS `suite`;
+CREATE TABLE IF NOT EXISTS `suite` (
+    `suiteId` int NOT NULL AUTO_INCREMENT,
+    `titre` varchar(50) NOT NULL,
+    `image` varchar(255) NOT NULL,
+    `descriptif` varchar(255) NOT NULL,
+    `prix` decimal(15, 2) NOT NULL,
+    `booking` varchar(100) DEFAULT NULL,
+    `etabId` int NOT NULL,
+    PRIMARY KEY (`suiteId`),
+    KEY `etabId` (`etabId`)
+  ) ENGINE = MyISAM AUTO_INCREMENT = 62 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+INSERT INTO
+  `suite` (
+    `suiteId`,
+    `titre`,
+    `image`,
+    `descriptif`,
+    `prix`,
+    `booking`,
+    `etabId`
+  )
+VALUES
+  (
+    59,
+    'Suite parentale',
+    'https://www.deco.fr/sites/default/files/styles/full_1200x630/public/migration-images/101525.jpg?itok=xm0oumy4',
+    'Une jolie suite parentale avec deux lits doubles',
+    '25.00',
+    'Book',
+    160
+  ),
+  (
+    60,
+    'Suite parentale',
+    'https://www.deco.fr/sites/default/files/styles/full_1200x630/public/migration-images/101525.jpg?itok=xm0oumy4',
+    'Une jolie suite parentale avec deux lits doubles',
+    '25.00',
+    'Book',
+    160
+  ),
+  (
+    61,
+    'Suite parentale',
+    'https://www.deco.fr/sites/default/files/styles/full_1200x630/public/migration-images/101525.jpg?itok=xm0oumy4',
+    'Une jolie suite parentale avec deux lits doubles',
+    '25.00',
+    'Book',
+    160
+  );
+DROP TABLE IF EXISTS `etablissement`;
+CREATE TABLE IF NOT EXISTS `etablissement` (
+    `etabId` int NOT NULL AUTO_INCREMENT,
+    `nom` varchar(50) NOT NULL,
+    `ville` varchar(50) NOT NULL,
+    `adresse` varchar(50) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `userId` int NOT NULL,
+    PRIMARY KEY (`etabId`),
+    UNIQUE KEY `userId` (`userId`)
+  ) ENGINE = MyISAM AUTO_INCREMENT = 171 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+INSERT INTO
+  `etablissement` (
+    `etabId`,
+    `nom`,
+    `ville`,
+    `adresse`,
+    `description`,
+    `userId`
+  )
+VALUES
+  (
+    160,
+    'Le relais du chateau',
+    'Chalon',
+    '15 rue de l\'eglise',
+    'Un joli petit hotel situé près d\'une rivière a 5 km du centre ville',
+    26
+  ),
+  (
+    170,
+    'La tour du pin',
+    'Marseille',
+    '25 impasse du bourg',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aspernatur molestias cum, optio totam doloremque nobis quisquam quam assumenda expedita magnam, veritatis sint asperiores itaque dignissimos eos quis. Vitae, minus.',
+    54
+  ),
+  (
+    169,
+    'La maison de papier',
+    'Paris',
+    '17 route de la paix',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aspernatur molestias cum, optio totam doloremque nobis quisquam quam assumenda expedita magnam, veritatis sint asperiores itaque dignissimos eos quis. Vitae, minus.',
+    58
+  ),
+  (
+    167,
+    'l\'Hotel de ville',
+    'Nantes',
+    '45 rue de la mairie',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quia perspiciatis temporibus saepe eum corporis sed facere tempore in. Cumque mollitia labore quaerat corporis libero neque officia fugit in error.',
+    56
+  ),
+  (
+    168,
+    'La maison de papier',
+    'Caen',
+    '12 avenue du 6 juin',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aspernatur molestias cum, optio totam doloremque nobis quisquam quam assumenda expedita magnam, veritatis sint asperiores itaque dignissimos eos quis. Vitae, minus.',
+    59
   );
